0f59f0829d5c17db46390b0fc852deef9c2de055
lukeo100 <l.topleyvlahos@boxhill.edu.au>
2025-06-17 10:21:13 +1000

    Revert "Created new data table component thats reusable and simpler."
    This reverts commit f2011ddbb04817b57a45e21d75e3f62d308b8f15.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,40 +3,6 @@
 	import SectionCards from '$lib/components/section-cards.svelte';
 	import ChartAreaInteractive from '$lib/components/chart-area-interactive.svelte';
 	import DataTable from '$lib/components/data-table.svelte';
-	import { type ColumnDef } from "@tanstack/table-core";
-
-	type DataItem = typeof data[0];
-
-	const columns: ColumnDef<DataItem>[] = [
-		{
-			accessorKey: 'id',
-			header: 'ID',
-		},
-		{
-			accessorKey: 'header',
-			header: 'Header',
-		},
-		{
-			accessorKey: 'type',
-			header: 'Type',
-		},
-		{
-			accessorKey: 'status',
-			header: 'Status',
-		},
-		{
-			accessorKey: 'target',
-			header: 'Target',
-		},
-		{
-			accessorKey: 'limit',
-			header: 'Limit',
-		},
-		{
-			accessorKey: 'reviewer',
-			header: 'Reviewer',
-		}
-	];
 </script>
 
 <div class="flex flex-1 flex-col">
@@ -46,9 +12,7 @@
 			<div class="px-4 lg:px-6">
 				<ChartAreaInteractive />
 			</div>
-			<div class="px-4 lg:px-6">
-				<DataTable {data} {columns} />
-			</div>
+			<DataTable {data} />
 		</div>
 	</div>
 </div>
